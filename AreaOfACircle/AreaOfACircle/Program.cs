fc774935c7f2aadb8fdb51a6e5c3331efc5e90f4
Combatd <combatdstar@gmail.com>
2021-01-31 06:38:52 -0800

    Finish AreaOfACircle terminal program

--- AreaOfACircle/AreaOfACircle/Program.cs
+++ AreaOfACircle/AreaOfACircle/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+We are going to write a program that will calculate the area of ​​a circle.
+The area enclosed by a circle of radius r is πr², where π (pi) is the constant ratio of the circumference of any circle to its diameter, and r is the radius.
+The given program declares a constant pi variable with a value of 3.14.
+Complete the program to take the radius as input, then calculate and output the area of the circle.
+*/
 
 namespace AreaOfACircle
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    class Program {
+        static void Main(string[] args) {
+            const double pi = 3.14;
+            double radius;
+
+            Console.WriteLine("Enter the radius: ");
+            radius = Convert.ToDouble(Console.ReadLine());
+            double area = pi * Math.Pow(radius, 2);
+            Console.WriteLine(area);
         }
     }
 }
