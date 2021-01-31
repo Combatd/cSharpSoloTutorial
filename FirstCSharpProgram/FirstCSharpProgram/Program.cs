eab502fd9dd5761586b2da802cd3fb2d1e8fd76c
Combatd <combatdstar@gmail.com>
2021-01-31 00:08:42 -0800

    Declare int-32 age

--- FirstCSharpProgram/FirstCSharpProgram/Program.cs
+++ FirstCSharpProgram/FirstCSharpProgram/Program.cs
@@ -4,12 +4,15 @@ namespace FirstCSharpProgram
 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("This asks for user input!");
+            //Console.WriteLine("This asks for user input!");
 
-            string temp;
-            temp = Console.ReadLine();
+            //string temp;
+            //temp = Console.ReadLine();
 
-            Console.WriteLine("variable temp value - {0}", temp);
+            //Console.WriteLine("variable temp value - {0}", temp);
+
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You are {0} years old in 32-bit integers", age);
         }
     }
 }
