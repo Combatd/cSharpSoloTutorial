42556a735288e17a6d91bea0a15eb6240cc8477e
Combatd <combatdstar@gmail.com>
2021-01-31 00:43:23 -0800

    Print name and age to Terminal

--- FirstCSharpProgram/FirstCSharpProgram/Program.cs
+++ FirstCSharpProgram/FirstCSharpProgram/Program.cs
@@ -11,8 +11,13 @@ namespace FirstCSharpProgram
 
             //Console.WriteLine("variable temp value - {0}", temp);
 
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("You are {0} years old in 32-bit integers", age);
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("You are {0} years old in 32-bit integers", age);
+
+            string name = Console.ReadLine();
+            string age = Console.ReadLine();
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + Convert.ToInt32(age));
         }
     }
 }
