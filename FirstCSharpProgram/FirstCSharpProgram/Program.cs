463c502d3636ecc3fabbfe2d036bd0ea05c1d5e1
Combatd <combatdstar@gmail.com>
2021-01-30 23:59:17 -0800

    Invoke Console.WriteLine for user input

--- FirstCSharpProgram/FirstCSharpProgram/Program.cs
+++ FirstCSharpProgram/FirstCSharpProgram/Program.cs
@@ -2,11 +2,14 @@
 
 namespace FirstCSharpProgram
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("This asks for user input!");
+
+            string temp;
+            temp = Console.ReadLine();
+
+            Console.WriteLine("variable temp value - {0}", temp);
         }
     }
 }
