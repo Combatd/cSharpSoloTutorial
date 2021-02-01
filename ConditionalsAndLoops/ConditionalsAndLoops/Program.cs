a84fccf1f94a5ba50dede68bca8e84553720a65e
Combatd <combatdstar@gmail.com>
2021-01-31 20:31:12 -0800

    Declare condition to check age

--- ConditionalsAndLoops/ConditionalsAndLoops/Program.cs
+++ ConditionalsAndLoops/ConditionalsAndLoops/Program.cs
@@ -4,9 +4,15 @@ namespace ConditionalsAndLoops
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args) {
+        
+            // Fill in the blanks to display Welcome to the screen when age is greater than 16.
+
+            int age = 24;
+            if (age > 16) {
+                Console.WriteLine("Welcome");
+            }
+            
         }
     }
 }
