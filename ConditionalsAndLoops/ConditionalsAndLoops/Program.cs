91765d61845e70d468f874802821919d78c11f59
Combatd <combatdstar@gmail.com>
2021-01-31 20:32:58 -0800

    Write condition to check larger of 2 variables

--- ConditionalsAndLoops/ConditionalsAndLoops/Program.cs
+++ ConditionalsAndLoops/ConditionalsAndLoops/Program.cs
@@ -5,14 +5,25 @@ namespace ConditionalsAndLoops
     class Program
     {
         static void Main(string[] args) {
-        
+
             // Fill in the blanks to display Welcome to the screen when age is greater than 16.
 
-            int age = 24;
-            if (age > 16) {
-                Console.WriteLine("Welcome");
+            //int age = 24;
+            //if (age > 16) {
+            //    Console.WriteLine("Welcome");
+            //}
+
+            // Fill in the blanks to find the larger of two variables.
+
+            int a = 42;
+            int b = 88;
+
+            if (a > b) {
+                Console.WriteLine(a);
+            } else {
+                Console.WriteLine(b);
             }
-            
+
         }
     }
 }
