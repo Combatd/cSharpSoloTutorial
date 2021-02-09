a15d2b36a68ccf0b954b4ceeee67ab0edcdf528f
Combatd <combatdstar@gmail.com>
2021-02-09 12:38:04 -0800

    Create MultipleOf3 project

---
+++ MultipleOf3/MultipleOf3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MultipleOf3
+{
+    /*
+     * You are an elementary school teacher and explaining multiplication to students.
+You     are going to use multiplication by 3 as your example.
+The     program you are given takes N number as input.
+        Write a program to output all numbers from 1 to N,
+        replacing all numbers that are multiples of 3 by "*".
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // your code goes here
+
+
+        }
+    }
+}
