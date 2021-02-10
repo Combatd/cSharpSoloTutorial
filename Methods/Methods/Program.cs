81109fb2c9046244ee02137400dd98cd12df46fa
Combatd <combatdstar@gmail.com>
2021-02-10 03:56:25 -0800

    Declare a static method that does not return a value

--- Methods/Methods/Program.cs
+++ Methods/Methods/Program.cs
@@ -6,7 +6,40 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             * A method is a group of statements that perform a particular task.
+                In addition to the C# built-in methods, you may also define your own.
+
+               Methods have many advantages, including:
+                - Reusable code.
+                - Easy to test.
+                - Modifications to a method do not affect the calling program.
+                - One method can accept many different inputs.
+             */
+
+            /*
+             Method Declaration
+
+            <return type> name(type1 par1, type2 par2, … , typeN parN)
+            {
+                List of statements
+            }
+
+
+            int Sqr(int x)
+            {
+              int result = x*x;
+              return result;
+            }
+
+             */
+
+            // Write a static method that does not return a value
+            static void Greet()
+            {
+                Console.WriteLine("Welcome");
+            }
+
         }
     }
 }
