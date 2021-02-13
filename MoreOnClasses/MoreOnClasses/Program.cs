a364a53b2276242aa646801454ff9932ed23a28f
Combatd <combatdstar@gmail.com>
2021-02-13 14:24:08 -0800

    Create MoreOnClasses namespace

---
+++ MoreOnClasses/MoreOnClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoreOnClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
