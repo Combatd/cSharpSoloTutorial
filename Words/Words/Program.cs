9b719c2f3e7a46863e900e80236662c5129012a5
Combatd <combatdstar@gmail.com>
2021-02-13 11:36:08 -0800

    Import boilerplate into Words program

--- Words/Words/Program.cs
+++ Words/Words/Program.cs
@@ -6,7 +6,22 @@ namespace Words
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] words = {
+                "home",
+                "programming",
+                "victory",
+                "C#",
+                "football",
+                "sport",
+                "book",
+                "learn",
+                "dream",
+                "fun"
+            };
+
+            string letter = Console.ReadLine();
+
+            int count = 0;
         }
     }
 }
