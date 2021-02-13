a5b024cdd677104f5caf68d76fb8790f952feffb
Combatd <combatdstar@gmail.com>
2021-02-13 15:16:54 -0800

    Declare class Dog

--- MoreOnClasses/MoreOnClasses/Program.cs
+++ MoreOnClasses/MoreOnClasses/Program.cs
@@ -6,7 +6,53 @@ namespace MoreOnClasses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             As constructors are used when a class is instantiated, destructors are automatically invoked when an object is destroyed or deleted.
+        Destructors have the following attributes:
+        - A class can only have one destructor.
+        - Destructors cannot be called. They are invoked automatically.
+        - A destructor does not take modifiers or have parameters.
+        - The name of a destructor is exactly the same as the class prefixed with a tilde (~).
+
+
+            class Dog
+            {
+              ~Dog() 
+              {
+                // code statements
+              }
+            }
+             */
+
+
+            /*
+             Destructors can be very useful for releasing resources before coming out of the program.
+             This can include closing files, releasing memory, and so on.
+             */
+
+
+
+            /*
+             Let’s include WriteLine statements in the destructor and constructor of our class and see how the program behaves when an object of that class is created and when the program ends:
+
+             This can be useful, for example, if your class is working with storage or files.
+             The constructor would initialize and open the files. Then, when the program ends, the destructor would close the files.
+             */
+
+            Dog d = new Dog();
+
+        }
+
+        class Dog
+        {
+            public Dog()
+            {
+                Console.WriteLine("Constructor");
+            }
+            ~Dog()
+            {
+                Console.WriteLine("Destructor");
+            }
         }
     }
 }
