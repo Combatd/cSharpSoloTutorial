431c44abd162e886a03a195a60c0cbe0107bdcab
Combatd <combatdstar@gmail.com>
2021-02-14 15:14:24 -0800

    Declare class DancerPoints

---
+++ Dance/Dance/DancerPoints.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Dance
+{
+    public class DancerPoints
+    {
+        public string name;
+        public int points;
+        public DancerPoints(string name, int points)
+        {
+            this.name = name;
+            this.points = points;
+        }
+
+        // overload the + operator
+    }
+}
