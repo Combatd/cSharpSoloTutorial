186f10c12ccb217d31e9108014744d3b068fd895
Combatd <combatdstar@gmail.com>
2021-02-14 13:55:21 -0800

    Declare class Person

--- MoreOnClasses/MoreOnClasses/Program.cs
+++ MoreOnClasses/MoreOnClasses/Program.cs
@@ -99,6 +99,118 @@ namespace MoreOnClasses
 
              */
             Dog.Bark();
+
+
+            /*
+             Constant members are static by definition.
+
+                        class MathClass {
+                            public const int ONE = 1;
+                        }
+
+                        static void Main(string[] args)
+                        {
+                            Console.Write(MathClass.ONE);
+                        }
+
+            As you can see, we access the property ONE using the name of the class, just like a static member.
+            This is because all const members are static by default.
+
+             */
+
+
+            /*
+             Constructors can be declared static to initialize static members of the class.
+             The static constructor is automatically called once when we access a static member of the class.
+
+
+                                class SomeClass {
+                                    public static int X { get; set; }
+                                    public static int Y { get; set; }
+            
+                                    static SomeClass() {
+                                        X = 10;
+                                        Y = 20;
+                                    }
+                                }
+                                static void Main(string[] args)
+                                {
+                                    Console.WriteLine(SomeClass.X);
+                                }
+
+            The constructor will get called once when we try to access SomeClass.X or SomeClass.Y.
+
+             */
+
+
+
+            /*
+                An entire class can be declared as static.
+                A static class can contain only static members.
+                You cannot instantiate an object of a static class, as only one instance of the static class can exist in a program.
+                Static classes are useful for combining logical properties and methods. A good example of this is the Math class.
+                It contains various useful properties and methods for mathematical operations.
+
+                Console.WriteLine(Math.Pow(2, 3));
+
+                You access all members of the Math class using the class name, without declaring an object.
+
+             */
+
+            /*
+
+                Math
+
+                Math.PI the constant PI.
+                Math.E represents the natural logarithmic base e.
+                Math.Max() returns the larger of its two arguments.
+                Math.Min() returns the smaller of its two arguments.
+                Math.Abs() returns the absolute value of its argument.
+                Math.Sin() returns the sine of the specified angle.
+                Math.Cos() returns the cosine of the specified angle.
+                Math.Pow() returns a specified number raised to the specified power.
+                Math.Round() rounds the decimal number to its nearest integral value.
+                Math.Sqrt() returns the square root of a specified number.
+
+
+                int[] arr = {1, 2, 3, 4};
+
+                Array.Reverse(arr);
+                //arr = {4, 3, 2, 1}
+
+                Array.Sort(arr);
+                //arr = {1, 2, 3, 4}
+
+
+
+                string s1 = "some text";
+                string s2 = "another text";
+
+                String.Concat(s1, s2); // combines the two strings
+
+                String.Equals(s1, s2); // returns false
+
+
+
+                Console.WriteLine(DateTime.Now);
+                Console.WriteLine(DateTime.Today);
+            
+                Console.WriteLine(DateTime.DaysInMonth(2016, 2));
+
+
+                The Console class is also an example of a static class. We use its static WriteLine() method to output to the screen, or the static ReadLine() method to get user input.
+                The Convert class used to convert value types is also a static class.
+
+             */
+
+            /*
+             The this keyword is used inside the class and refers to the current instance of the class,
+             meaning it refers to the current object.
+             One of the common uses of this is to distinguish class members from other data,
+             such as local or formal parameters of a method
+             */
+
+
         }
 
         class Dog
@@ -116,6 +228,15 @@ namespace MoreOnClasses
             public static void Bark()
             {
                 Console.WriteLine("Woof");
+            }
+        }
+
+        class Person
+        {
+            private string name;
+            public Person(string name)
+            { // this.name represents the member of the class, whereas name represents the parameter of the constructor.
+                this.name = name;
             }
         }
     }
