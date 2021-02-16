6c71c9e56467f9ee03f40f0840c4154c8e3da3b1
Combatd <combatdstar@gmail.com>
2021-02-16 15:52:48 -0800

    Create CoffeeTime Project

---
+++ CoffeeTime/CoffeeTime/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoffeeTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
