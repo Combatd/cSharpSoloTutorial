bb2b1432a8ba5b8fdacbd0d198c0b8998cbe0401
Combatd <combatdstar@gmail.com>
2021-02-16 13:34:44 -0800

    Declare generic class Stack

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -77,11 +77,49 @@ namespace Generics
             string y = "test";
             Func(x, y);
 
+
+            /*
+             Generic types can also be used with classes.
+            The most common use for generic classes is with collections of items, where operations such as adding and removing items from the collection are performed in basically the same way regardless of the type of data being stored. One type of collection is called a stack. Items are "pushed", or added to the collection, and "popped", or removed from the collection.
+            A stack is sometimes called a Last In First Out (LIFO) data structure.
+
+            The generic class stores elements in an array.
+            As you can see, the generic type T is used as the type of the array,
+            the parameter type for the Push method, and the return type for the Pop and Get methods.
+
+
+            We can also use the generic class with custom types, such as the custom defined Person type.
+
+
+            In a generic class we do not need to define the generic type for its methods,
+            because the generic type is already defined on the class level.
+
+             */
+
+            Stack<int> intStack = new Stack<int>();
+            Stack<string> strStack = new Stack<string>();
+            // Stack<Person> PersonStack = new Stack<Person>();
+
         }
 
         static void Func<T, U> (T x , U y)
         {
             Console.WriteLine(x + " " + y);
+        }
+
+        class Stack<T>
+        {
+            int index = 0;
+            T[] innerArray = new T[100];
+            public void Push(T item)
+            {
+                innerArray[index++] = item;
+            }
+            public T Pop()
+            {
+                return innerArray[--index];
+            }
+            public T Get(int k) { return innerArray[k]; }
         }
 
     }
